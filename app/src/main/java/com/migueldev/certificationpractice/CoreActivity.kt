c38711e6f370b2c3d9e3b2bb432f45b63afa1751
Miguel <belloingmiguel@gamil.com>
2022-01-06 09:50:39 -0500

    init commit

---
+++ app/src/main/java/com/migueldev/certificationpractice/CoreActivity.kt
@@ -0,0 +1,11 @@
+package com.migueldev.certificationpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_core)
+    }
+}
---
+++ app/src/main/java/com/migueldev/certificationpractice/UserInterfaceActivity.kt
@@ -0,0 +1,11 @@
+package com.migueldev.certificationpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class UserInterfaceActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_interface)
+    }
+}
